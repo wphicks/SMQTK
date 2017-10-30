0e4ab05da51bee7c0b763504d060db36fea53db3
William Hicks <william.p.hicks@gmail.com>
2017-10-30 15:17:46 -0400

    Parameterize hash index benchmarks

--- python/smqtk/benchmarks/algorithms/nn_index/hash_index/test_hash_indices.py
+++ python/smqtk/benchmarks/algorithms/nn_index/hash_index/test_hash_indices.py
@@ -4,115 +4,82 @@ from smqtk.algorithms.nn_index.hash_index.sklearn_balltree import \
     SkLearnBallTreeHashIndex
 from smqtk.algorithms.nn_index.hash_index.vptree import VPTreeHashIndex
 
-random_seed = 0
-sample_size = 10**4
-dimension = 4096
-hashes = numpy.random.randint(
-    0, high=2, size=(sample_size, dimension), dtype='bool')
-btree = SkLearnBallTreeHashIndex(random_seed=random_seed)
-vptree = VPTreeHashIndex(random_seed=random_seed, tree_type="vp")
-vpstree = VPTreeHashIndex(random_seed=random_seed, tree_type="vps")
-vpsbtree = VPTreeHashIndex(random_seed=random_seed, tree_type="vpsb")
-btree.build_index(hashes)
-vptree.build_index(hashes)
-vpstree.build_index(hashes)
-vpsbtree.build_index(hashes)
+_index_type_dict = {"BallTree": (SkLearnBallTreeHashIndex, {"random_seed": 0}),
+                    "VPTree": (VPTreeHashIndex, {"random_seed": 0,
+                                                 "tree_type": "vp"}),
+                    "VPSTree": (VPTreeHashIndex, {"random_seed": 0,
+                                                  "tree_type": "vps"}),
+                    "VPSBTree": (VPTreeHashIndex, {"random_seed": 0,
+                                                   "tree_type": "vpsb"})}
 
-random_query_vec = numpy.random.randint(0, high=2, size=dimension,
-                                        dtype='bool')
-exact_query_vec = hashes[numpy.random.randint(hashes.shape[0])]
-near_query_vec = hashes[numpy.random.randint(hashes.shape[0])]
-change_index = numpy.random.randint(near_query_vec.shape[0])
-near_query_vec[change_index] = not near_query_vec[change_index]
+indices = sorted(_index_type_dict.keys())
+sample_sizes = [10**3, 10**6, 10**7, 10**8]
+dimensions = [4096]
+random_seeds = [0]
 
 
+@pytest.mark.parametrize('sample_size', sample_sizes)
+@pytest.mark.parametrize('dimension', dimensions)
+@pytest.mark.parametrize('random_seed', random_seeds)
+@pytest.mark.parametrize('index', indices)
 @pytest.mark.benchmark(group="build_index")
-def test_bt_build(benchmark):
-    benchmark(SkLearnBallTreeHashIndex(random_seed=random_seed).build_index,
-              hashes)
+def test_build(benchmark, sample_size, dimension, random_seed, index):
+    index = _index_type_dict[index]
+    index = index[0](**index[1])
+    numpy.random.seed(random_seed)
+    hashes = numpy.random.randint(0, high=2,
+                                  size=(sample_size, dimension), dtype='bool')
+    benchmark(index.build_index, hashes)
 
 
-@pytest.mark.benchmark(group="build_index")
-def test_vp_build(benchmark):
-    benchmark(VPTreeHashIndex(random_seed=random_seed,
-              tree_type="vp").build_index, hashes)
-
-
-@pytest.mark.benchmark(group="build_index")
-def test_vps_build(benchmark):
-    benchmark(VPTreeHashIndex(random_seed=random_seed,
-              tree_type="vps").build_index, hashes)
-
-
-@pytest.mark.benchmark(group="build_index")
-def test_vpsb_build(benchmark):
-    benchmark(VPTreeHashIndex(random_seed=random_seed,
-              tree_type="vpsb").build_index, hashes)
-
-
-@pytest.mark.benchmark(group="nn_random")
-def test_bt_nn_random(benchmark):
-    benchmark(btree.nn, random_query_vec)
-
-
-@pytest.mark.benchmark(group="nn_random")
-def test_vp_nn_random(benchmark):
-    benchmark(vptree.nn, random_query_vec)
-
-
-@pytest.mark.benchmark(group="nn_random")
-def test_vps_nn_random(benchmark):
-    benchmark(vpstree.nn, random_query_vec)
-
-
-@pytest.mark.benchmark(group="nn_random")
-def test_vpsb_nn_random(benchmark):
-    benchmark(vpsbtree.nn, random_query_vec)
-
-
+@pytest.mark.parametrize('sample_size', sample_sizes)
+@pytest.mark.parametrize('dimension', dimensions)
+@pytest.mark.parametrize('random_seed', random_seeds)
+@pytest.mark.parametrize('index', indices)
 @pytest.mark.benchmark(group="nn_exact")
-def test_bt_nn_exact(benchmark):
-    result = benchmark(btree.nn, exact_query_vec)
-    numpy.testing.assert_equal(result[0][0], exact_query_vec)
+def test_nn_exact(benchmark, sample_size, dimension, random_seed, index):
+    index = _index_type_dict[index]
+    index = index[0](**index[1])
+    numpy.random.seed(random_seed)
+    hashes = numpy.random.randint(0, high=2,
+                                  size=(sample_size, dimension), dtype='bool')
+    query_vec = hashes[numpy.random.randint(hashes.shape[0])]
+    index.build_index(hashes)
+    result = benchmark(index.nn, query_vec)
+    numpy.testing.assert_equal(result[0][0], query_vec)
 
 
-@pytest.mark.benchmark(group="nn_exact")
-def test_vp_nn_exact(benchmark):
-    result = benchmark(vptree.nn, exact_query_vec)
-    numpy.testing.assert_equal(result[0][0], exact_query_vec)
+@pytest.mark.parametrize('sample_size', sample_sizes)
+@pytest.mark.parametrize('dimension', dimensions)
+@pytest.mark.parametrize('random_seed', random_seeds)
+@pytest.mark.parametrize('index', indices)
+@pytest.mark.benchmark(group="nn_random")
+def test_nn_random(benchmark, sample_size, dimension, random_seed, index):
+    index = _index_type_dict[index]
+    index = index[0](**index[1])
+    numpy.random.seed(random_seed)
+    hashes = numpy.random.randint(0, high=2,
+                                  size=(sample_size, dimension), dtype='bool')
+    query_vec = numpy.random.randint(0, high=2, size=dimension,
+                                     dtype='bool')
+    index.build_index(hashes)
+    benchmark(index.nn, query_vec)
 
 
-@pytest.mark.benchmark(group="nn_exact")
-def test_vps_nn_exact(benchmark):
-    result = benchmark(vpstree.nn, exact_query_vec)
-    numpy.testing.assert_equal(result[0][0], exact_query_vec)
-
-
-@pytest.mark.benchmark(group="nn_exact")
-def test_vpsb_nn_exact(benchmark):
-    result = benchmark(vpsbtree.nn, exact_query_vec)
-    numpy.testing.assert_equal(result[0][0], exact_query_vec)
-
-
+@pytest.mark.parametrize('sample_size', sample_sizes)
+@pytest.mark.parametrize('dimension', dimensions)
+@pytest.mark.parametrize('random_seed', random_seeds)
+@pytest.mark.parametrize('index', indices)
 @pytest.mark.benchmark(group="nn_near")
-def test_bt_nn_near(benchmark):
-    result = benchmark(btree.nn, near_query_vec)
-    assert result[1][0] < 1.1 / near_query_vec.shape[0]
-
-
-@pytest.mark.benchmark(group="nn_near")
-def test_vp_nn_near(benchmark):
-    result = benchmark(vptree.nn, near_query_vec)
-    assert result[1][0] < 1.1 / near_query_vec.shape[0]
-
-
-@pytest.mark.benchmark(group="nn_near")
-def test_vps_nn_near(benchmark):
-    result = benchmark(vpstree.nn, near_query_vec)
-    assert result[1][0] < 1.1 / near_query_vec.shape[0]
-
-
-@pytest.mark.benchmark(group="nn_near")
-def test_vpsb_nn_near(benchmark):
-    result = benchmark(vpsbtree.nn, near_query_vec)
-    assert result[1][0] < 1.1 / near_query_vec.shape[0]
+def test_nn_near(benchmark, sample_size, dimension, random_seed, index):
+    index = _index_type_dict[index]
+    index = index[0](**index[1])
+    numpy.random.seed(random_seed)
+    hashes = numpy.random.randint(0, high=2,
+                                  size=(sample_size, dimension), dtype='bool')
+    query_vec = hashes[numpy.random.randint(hashes.shape[0])]
+    change_index = numpy.random.randint(query_vec.shape[0])
+    query_vec[change_index] = not query_vec[change_index]
+    index.build_index(hashes)
+    result = benchmark(index.nn, query_vec)
+    assert result[1][0] < 1.1 / query_vec.shape[0]
